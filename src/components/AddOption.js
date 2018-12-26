fa9a8ada2fba318f739349aa4a1442e80ddc3613
QC <cheng305@purdue.edu>
2018-12-26 15:05:32 -0500

    webpack class properties plugins

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
 export default class AddOption extends React.Component {
+    state = {
+        error: undefined
+    };
 
     constructor(props){
         super(props);
-        this.onSubmit = this.onSubmit.bind(this);
-        this.state = {
-            error: undefined
-        }
     }
 
-    onSubmit(e) {
+    onSubmit = (e) => {
         e.preventDefault();
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
